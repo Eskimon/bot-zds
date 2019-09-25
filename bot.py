4e39b56e09ab440303ab499ce8894b11dde01df9
Eskimon <eskimon@outlook.com>
2019-09-25 10:00:19 +0200

    fix command cherche

--- bot.py
+++ bot.py
@@ -12,8 +12,10 @@ QUERY_SEARCH = 'https://zestedesavoir.com/rechercher/?q={query}'
 
 
 def get_search_results(query):
+    query = urllib.parse.quote_plus(query)
+    print(QUERY_SEARCH.format(query=query))
     req = request.Request(
-        QUERY_SEARCH.format(query='Arduino'),
+        QUERY_SEARCH.format(query=query),
         data=None,
         headers={
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'  # noqa
@@ -30,16 +32,23 @@ def get_search_results(query):
         return []
 
     results = soup.find_all("article", class_="content-item")
-    tag = results[0]
-    info = tag.find_all("div", class_="content-info")[0]
-    link = info.find_all("a")[0]
-    desc = tag.find_all("p", class_="content-description")[0]
-    return [{
-        'thumbnail': tag.find_all('img')[0]['src'],
-        'url': link['href'],
-        'title': link.get_text().strip(),
-        'description': desc.get_text().strip(),
-    }]
+    if results:
+        tag = results[0]
+        info = tag.find_all("div", class_="content-info")[0]
+        link = info.find_all("a")[0]
+        desc = tag.find_all("p", class_="content-description")[0]
+        try:
+            thumbnail = tag.find_all('img')[0]['src']
+        except IndexError:
+            thumbnail = None
+        return [{
+            'thumbnail': thumbnail,
+            'url': link['href'],
+            'title': link.get_text().strip(),
+            'description': desc.get_text().strip(),
+        }]
+    else:
+        return []
 
 
 class MyClient(discord.Client):
@@ -55,8 +64,8 @@ class MyClient(discord.Client):
             await message.channel.send(zeste)
 
         if message.content.startswith('!cherche'):
-            query = message.content[8:]
-            result = get_search_results(' '.join(query))
+            query = message.content[len('!cherche '):]
+            result = get_search_results(query)
             if result:
                 result = result[0]
                 embed = discord.Embed(
@@ -65,7 +74,8 @@ class MyClient(discord.Client):
                     description=result['description'],
                     url=ROOT_URL + result['url'],
                 )
-                embed.set_thumbnail(url=ROOT_URL + result['thumbnail'])
+                if result['thumbnail']:
+                    embed.set_thumbnail(url=ROOT_URL + result['thumbnail'])
                 await message.channel.send(content='Voici ce que j\'ai trouvé !', embed=embed)
             else:
                 await message.channel.send(':clemtriste: Je n\'ai rien trouvé à ce sujet.')
